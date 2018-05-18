3d1231fca0d088edddb7dd03930ee5643ce27584
altooma <andrelagreca1@gmail.com>
2018-05-18 13:50:15 +1000

    added day counter
    Signed-off-by: altooma <andrelagreca1@gmail.com>

---
+++ Assets/Day Night Cycle/timeCycle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeCycle : MonoBehaviour {
+
+    private float dayLength = 1200f;
+    private float nightLength = 600f;
+
+    private int day;
+    private int night;
+
+    private float currentTime;
+
+    public int days;
+
+	void Update () {
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= dayLength && day == 0)
+        {
+            day = 1;
+            currentTime = 0;
+        }
+        if (currentTime >= nightLength && night == 0 && day == 1)
+        {
+            night = 1;
+            currentTime = 0;
+        }
+        if (night == 1 && day == 1)
+        {
+            night = 0;
+            day = 0;
+            days += 1;
+        }
+	}
+}
